dfabda3f9d58a14ec4e8e7c88cbd4bf664526125
Douglas Molina <douglas.molina@va.gov>
2020-06-11 22:51:54 -0500

    Update PySilo-1.py
    Hope this works

--- PySilo-1.py
+++ PySilo-1.py
@@ -37,29 +37,6 @@ from model.PySiloReport import PySiloReport
 urllib3.disable_warnings()
 
 
-# Error handling function.
-def error_handling(e, a_string):
-    """
-    :param e:
-    :type e:
-    :param a_string:
-    :type a_string:
-    :return:
-    :rtype:
-    """
-    if hasattr(e, 'message'):
-        PySilo_settings.logger.debug('%s %s', a_string, e.message)
-    import traceback
-    PySilo_settings.logger.debug('1 %s', e.__doc__)
-    PySilo_settings.logger.debug('2 %s', sys.exc_info())
-    PySilo_settings.logger.debug('3 %s', sys.exc_info()[0])
-    PySilo_settings.logger.debug('4 %s', sys.exc_info()[1])
-    #PySilo_settings.logger.debug('5 %s', sys.exc_info()[2], 'Sorry I mean line...',
-    #                              traceback.tb_lineno(sys.exc_info()[2]))
-    ex_type, ex, tb = sys.exc_info()
-    PySilo_settings.logger.debug('6 %s', traceback.print_tb(tb))
-    return sys, traceback
-
 
 # This function is not used!
 def misp_delete_events(a_start, a_end, a_misp_instance):
@@ -111,13 +88,62 @@ def isight_search_indicators(base_url, public_key, private_key, hours):
     # Fetch the data
 
     final_url = base_url + "&apiKey=" + apiKey + "&digest=" + urllib.parse.quote(digest.decode())
-    req = urllib.request.Request(final_url)                                                           
+    req = urllib.request.Request(final_url) 
+    return(req)
 
     # Retrieve indicators and warning data since the specified date and time.
-    return isight_prepare_data_request(base_url, search_query, public_key, private_key)
+    #return isight_prepare_data_request(base_url, search_query, public_key, private_key)
+def fetch(a_result)
+   with urllib.request.urlopen(a_result) as response:
+     responseJson = response.read()
+     return(respnseJson)
+    
+def misp_process_isight_indicators(a_result):
+    """
+    :param a_result:
+    :type a_result:
+    """
+    # Process each indicator in the JSON message
+    for indicator in a_result['message']:
+        Print("#####indicator#####",indicator)
+        PySilo_settings.logger.debug('Processing report %s', indicator['reportId'])
 
-        
-        
+        if PySilo_settings.use_threading:
+            # Use threads to process the indicators
+            print('***threading****')
+            # First, set the maximum number of threads
+            thread_limiter = threading.BoundedSemaphore(value=PySilo_settings.number_threads)
+            # Define a thread
+            t = threading.Thread(target=process_isight_indicator, args=(indicator,))
+            # Start the thread
+            t.start()
+        else:
+            # No threading
+            print('***no threading***')
+            process_isight_indicator(indicator)
+
+def isight_prepare_data_request(a_url, a_query, a_pub_key, a_prv_key):
+    """
+    :param a_url:
+    :type a_url:
+    :param a_query:
+    :type a_query:
+    :param a_pub_key:
+    :type a_pub_key:
+    :param a_prv_key:
+    :type a_prv_key:
+    :return:
+    :rtype:
+    """
+    header = set_header(a_prv_key, a_pub_key, a_query)
+    result = isight_load_data(a_url, a_query, header)
+    print('######header:',header)
+    print('#####result:',result)
+    if not result:
+        PySilo_settings.logger.debug('Something went wrong when retrieving indicators from the FireEye iSight API')
+        return False
+    else:
+        return result    
         
 ###################################################start###############################        
  if __name__ == '__main__':
@@ -134,14 +160,20 @@ def isight_search_indicators(base_url, public_key, private_key, hours):
     result = isight_search_indicators(PySilo_settings.isight_url, PySilo_settings.SharedKey,PySilo_settings.APIKey, PySilo_settings.isight_last_hours)
   # PySilo_settings.logger.debug("url:",PySilo_settings.isight_url,"shared key:", PPySilo_settings.SharedKey,"APIKey:",PySilo_settings.APIKey,"hrs",PySilo_settings_last_hours)
     print('####result####',result)
+    #fetch(result)
     PySilo_settings.logger.debug('####result####',result)
     if result is False:
         PySilo_settings.logger.debug('No informatopm available from SiloBreaker')
     else:
-        misp_process_isight_indicators(result)
+        json=fetch(result)
+        #misp_process_isight_indicators(json)
 
     PySilo_settings.logger.debug('PySilo2MISP finished at %s', datetime.datetime.now(datetime.timezone.utc))
     # If loglevel equals DEBUG, log the time the script ran.
+    Print('####Print data and figure it out later#####')
+    # Pretty print the data
+    responseObject = json.loads(responseJson.decode("utf-8"))
+    print(json.dumps(responseObject, sort_keys=True, indent=2, separators=(',', ': ')))
     if PySilo_settings.debug_mode:
         end = timer()
         PySilo_settings.logger.debug('Time taken %s', end - start)
